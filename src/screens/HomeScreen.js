47e4a9ddb8bad5515d374c15880609ecba82de97
thyagoneck <thyagoneck@gmail.com>
2020-03-01 11:46:34 -0300

    feat: preview e reducer

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "border-top-left-radius":
+      return { ...state, "border-top-left-radius": action.payload };
+    case "border-top-right-radius":
+      return { ...state, "border-top-right-radius": action.payload };
+    case "border-bottom-right-radius":
+      return { ...state, "border-bottom-right-radius": action.payload };
+    case "border-bottom-left-radius":
+      return { ...state, "border-bottom-left-radius": action.payload };
+
+    default:
+      return state;
+  }
+};
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, {
+    "border-top-left-radius": 0,
+    "border-top-right-radius": 0,
+    "border-bottom-right-radius": 0,
+    "border-bottom-left-radius": 0
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.previewStyle} />
     </View>
   );
 }
@@ -15,5 +38,10 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
+  },
+  previewStyle: {
+    height: 100,
+    width: 100,
+    backgroundColor: "purple"
   }
 });
